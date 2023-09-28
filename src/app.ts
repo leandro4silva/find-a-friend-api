d2b93a9f861df15c42f4b2fc6dbb248243d891fe
leandro4silva <leandro94.a.s@gmail.com>
2023-09-28 20:46:19 -0300

    feat: authenticate factorie and controller

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,19 @@
 import fastify from "fastify";
+import { env } from "./env";
+import { fastifyJwt } from "@fastify/jwt";
 import { petsRoutes } from "./http/controllers/pets/routes";
 
 export const app = fastify();
+
+app.register(fastifyJwt, {
+  secret: env.NODE_ENV,
+  cookie: {
+    cookieName: "refreshToken",
+    signed: false,
+  },
+  sign: {
+    expiresIn: "10m",
+  },
+});
 
 app.register(petsRoutes);
