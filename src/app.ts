fda5fbaeec3b38d70cdcb87a0aca2dcf10a465a1
leandro4silva <leandro94.a.s@gmail.com>
2023-10-02 21:51:17 -0300

    feat: zod error

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import fastify from "fastify";
 import { env } from "./env";
 import { fastifyJwt } from "@fastify/jwt";
 import { petsRoutes } from "./http/controllers/pets/routes";
+import { orgsRoutes } from "./http/controllers/orgs/routes";
+import { ZodError } from "zod";
 
 export const app = fastify();
 
@@ -17,3 +19,20 @@ app.register(fastifyJwt, {
 });
 
 app.register(petsRoutes);
+app.register(orgsRoutes);
+
+app.setErrorHandler((error, _request, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: "Validation error", issues: error.format() });
+  }
+
+  if (env.NODE_ENV !== "production") {
+    console.error(error);
+  } else {
+    // TODO Here we should log to an external tool like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: "Internal server error" });
+});
