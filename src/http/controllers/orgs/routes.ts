88d691f96d98809e9c8a66e9f503415102d667e3
leandro4silva <leandro94.a.s@gmail.com>
2023-10-06 00:00:20 -0300

    feat: use case whatsapp

--- src/http/controllers/orgs/routes.ts
+++ src/http/controllers/orgs/routes.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { authenticate } from "./authenticate";
 import { create } from "./create";
+import { whatsapp } from "./whatsapp";
 
 export async function orgsRoutes(app: FastifyInstance) {
   app.post("/sessions", authenticate);
   app.post("/orgs", create);
+  app.get("/orgs/whatsapp", whatsapp);
 }
--- src/http/controllers/orgs/whatsapp.spec.ts
+++ src/http/controllers/orgs/whatsapp.spec.ts
@@ -1,6 +1,7 @@
 import { app } from "@/app";
 import { describe, beforeAll, afterAll, it, expect } from "vitest";
 import request from "supertest";
+import { createAndAithenticateOrg } from "@/utils/test/create-and-authenticate-org";
 
 describe("Authenticate (e2e)", async () => {
   beforeAll(async () => {
@@ -11,23 +12,19 @@ describe("Authenticate (e2e)", async () => {
     await app.ready();
   });
 
-  it("should be able to authenticate", async () => {
-    await request(app.server).post("/orgs").send({
-      name: "John Doe",
-      email: "johndoe@example.com",
-      zipCode: "13385074",
-      whatsapp: "19999999999",
-      password: "1234567",
-    });
+  it("should be able to get whatsapp from org", async () => {
+    const { token } = await createAndAithenticateOrg(app);
 
-    const response = await request(app.server).post("/sessions").send({
-      email: "johndoe@example.com",
-      password: "1234567",
-    });
+    const response = await request(app.server)
+      .get("/orgs/whatsapp")
+      .set("Authorization", `Bearer ${token}`)
+      .send();
 
     expect(response.statusCode).toEqual(200);
-    expect(response.body).toEqual({
-      token: expect.any(String),
-    });
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        email: "johndoe@example.com",
+      }),
+    );
   });
 });
